e80ceab9aba4abcab484c9883762d040d8e9ad56
javier Camilo <119640318+cryptojavier@users.noreply.github.com>
2023-08-25 20:41:43 -0500

    ✨ add some test of readContract

--- tests/utils/readContract.test.ts
+++ tests/utils/readContract.test.ts
@@ -4,7 +4,7 @@ import * as readContract from '../../src/shared/utils/readContract';
 describe("readContract", () => {
 
     it("newAuctionsValue", async () => {
-        const mockCall = jest.fn().mockResolvedValue("2000"); // Mockear el método call de erc20_contract
+        const mockCall = jest.fn().mockResolvedValue("2000");
         const mockErc20Contract = {
           methods: {
             allowance: jest.fn(() => ({ call: mockCall }))
@@ -16,13 +16,42 @@ describe("readContract", () => {
     });
 
     it("newAuctionsValue > wrong addres", async () => {
-        const mockCall = jest.fn().mockResolvedValue("2000"); // Mockear el método call de erc20_contract
+        const mockCall = jest.fn().mockResolvedValue("2000");
         // Llamar a la función newAuctionsValue
         const t = () => {
             throw new TypeError("Error AuctionsValue");
         };
         try {
             const result = await readContract.newAuctionsValue("0x2B4");
+        } catch (error) {
+            expect(t).toThrow(TypeError);
+        }
+    });
+
+    it("getBalanceERC20", async () => {
+        const mockCall = jest.fn().mockResolvedValue("2000");
+        const mockErc20Contract = {
+          methods: {
+            allowance: jest.fn(() => ({ call: mockCall }))
+          }
+        };
+        // Llamar a la función newAuctionsValue
+        const result = await readContract.getBalanceERC20("0x2B4BB6B8512Ae5363F03DB53Fd20CC7C464168C9");
+        expect(result).toBe(1000999993000n);
+    });
+    it("validateFound", async () => {
+        const mockCall = jest.fn().mockResolvedValue("2000");
+        const mockErc20Contract = {
+          methods: {
+            allowance: jest.fn(() => ({ call: mockCall }))
+          }
+        };
+        
+        const t = () => {
+            throw new TypeError("You not have funds for this token");
+        };
+        try {
+            const result = await readContract.validateFound(30,10);
         } catch (error) {
             expect(t).toThrow(TypeError);
         }
